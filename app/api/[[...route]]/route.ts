8dd49f2b0e418e5e9e8cdca6535cee5011f30278
Akshay <akshay.thool@gmail.com>
2024-09-19 10:17:55 +0530

    Add Header Component

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,14 +1,12 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
-import authors from './authors'
-import books from './books'
 
 export const runtime = 'edge'
 
 const app = new Hono().basePath('/api')
 
-app.route('/authors', authors)
-.route('/books', books)
+app.get('/hello', (c) => {
+  return c.json('test hello', 200);
+})
 
 export const GET = handle(app)
-export const POST = handle(app)
