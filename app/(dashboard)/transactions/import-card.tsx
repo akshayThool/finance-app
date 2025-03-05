7e44c1f6a866a2882af599356a38f877c7d72242
Akshay <akshay.thool@gmail.com>
2025-03-05 21:56:48 +0530

    Update Import Transaction Page

---
+++ app/(dashboard)/transactions/import-card.tsx
@@ -0,0 +1,130 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { ImportTable } from "./import-table";
+import { useState } from "react";
+import { convertAmountToMiliunits } from "@/lib/utils";
+import { format, parse } from "date-fns";
+
+type Props = {
+  data: string[][];
+  onCancel: () => void;
+};
+
+interface SelectedColumnState {
+  [key: string]: string | null;
+}
+
+const requiredOptions = ["date", "payee", "amount"];
+
+const dateFormat = "yyyy-MM-dd HH:mm:ss";
+const outputFormat = "yyyy-MM-dd";
+
+export const ImportCard = ({ data, onCancel }: Props) => {
+  const header = data[0];
+  const body = data.slice(1);
+
+  const [selectedColumns, setSelectedColumns] = useState<SelectedColumnState>(
+    {}
+  );
+
+  const onTableHeadSelectChange = (
+    columnIndex: number,
+    value: string | null
+  ) => {
+    setSelectedColumns((prev) => {
+      const newSelectedColumns = { ...prev };
+
+      for (const key in newSelectedColumns) {
+        if (newSelectedColumns[key] === value) {
+          newSelectedColumns[key] = null;
+        }
+      }
+
+      if (value === "skip") {
+        value = null;
+      }
+
+      newSelectedColumns[`column_${columnIndex}`] = value;
+
+      return newSelectedColumns;
+    });
+  };
+
+  const progress = Object.values(selectedColumns).filter(Boolean).length;
+
+  const onHandleContinue = () => {
+    const getColumnIndex = (index: number) => {
+      return `column_${index}`;
+    };
+
+    const mappedDate = {
+      headers: header.map((_header, index) => {
+        return selectedColumns[getColumnIndex(index)] || null;
+      }),
+      body: body
+        .map((row) => {
+          const transformedRow = row.map((cell, index) => {
+            return selectedColumns[getColumnIndex(index)] ? cell : null;
+          });
+
+          return transformedRow.every((item) => item === null)
+            ? []
+            : transformedRow;
+        })
+        .filter((row) => row.length > 0),
+    };
+
+    const arrayOfData = mappedDate.body.map((row) => {
+      return row.reduce((acc: any, cell, index) => {
+        const header = mappedDate.headers[index];
+
+        if (header !== null) {
+          acc[header] = cell;
+        }
+
+        return acc;
+      }, {});
+    });
+
+    const formattedData = arrayOfData.map((item) => ({
+      ...item,
+      amount: convertAmountToMiliunits(parseFloat(item.amount)),
+      date: format(parse(item.date, dateFormat, new Date()), outputFormat),
+    }));
+
+    console.log({ formattedData });
+  };
+
+  return (
+    <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+      <Card className="border-none drop-shadow-sm">
+        <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
+          <CardTitle className="text-xl line-clamp-1">
+            Transaction History
+          </CardTitle>
+          <div className="flex items-center gap-2 flex-col lg:flex-row">
+            <Button size="sm" onClick={onCancel} className="w-full lg:w-auto">
+              Cancel
+            </Button>
+            <Button
+              size="sm"
+              disabled={progress < requiredOptions.length}
+              className="w-full lg:w-auto"
+              onClick={onHandleContinue}
+            >
+              Continue {progress} / {requiredOptions.length}
+            </Button>
+          </div>
+        </CardHeader>
+        <CardContent>
+          <ImportTable
+            header={header}
+            body={body}
+            selectedColumns={selectedColumns}
+            onTableHeadSelectChange={onTableHeadSelectChange}
+          />
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
--- app/(dashboard)/transactions/page.tsx
+++ app/(dashboard)/transactions/page.tsx
@@ -1,32 +1,61 @@
 "use client";
 
 import { useGetTransactions } from "@/features/transactions/api/use-get-transactions";
-import {useDeleteTransactions} from "@/features/transactions/api/use-delete-transactions";
+import { useDeleteTransactions } from "@/features/transactions/api/use-delete-transactions";
 import { useNewTransaction } from "@/features/transactions/hooks/use-new-transaction";
 
-import {Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card";
-import {Loader2, Plus} from "lucide-react";
-import {columns} from "@/app/(dashboard)/transactions/columns";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Loader2, Plus } from "lucide-react";
+import { columns } from "@/app/(dashboard)/transactions/columns";
 
-import {DataTable} from "@/components/data-table";
-import {Button} from "@/components/ui/button";
-import {Skeleton} from "@/components/ui/skeleton";
+import { DataTable } from "@/components/data-table";
+import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
+
+import { UploadButton } from "./upload-button";
+import { useState } from "react";
+import { ImportCard } from "./import-card";
+
+enum VARIANTS {
+  LIST = "LIST",
+  IMPORT = "IMPORT",
+}
+
+const INITIAL_IMPORT_RESULTS = {
+  data: [],
+  errors: [],
+  meta: {},
+};
 
 const TransactionPage = () => {
+  const [variant, setVariant] = useState<VARIANTS>(VARIANTS.LIST);
+  const [importResults, setImportResults] = useState(INITIAL_IMPORT_RESULTS);
+
+  const onUploadCSV = (results: typeof INITIAL_IMPORT_RESULTS) => {
+    setImportResults(results);
+    setVariant(VARIANTS.IMPORT);
+  };
+
+  const onCancelImport = () => {
+    setVariant(VARIANTS.LIST);
+    setImportResults(INITIAL_IMPORT_RESULTS);
+  };
+
   const newTransaction = useNewTransaction();
   const getAllTransactions = useGetTransactions();
   const deleteTransactions = useDeleteTransactions();
 
   const transactions = getAllTransactions.data || [];
 
-  const isDisabled = getAllTransactions.isLoading || deleteTransactions.isPending;
+  const isDisabled =
+    getAllTransactions.isLoading || deleteTransactions.isPending;
 
-  if(getAllTransactions.isLoading){
+  if (getAllTransactions.isLoading) {
     return (
       <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
         <Card className="border-none drop-shadow-sm">
           <CardHeader>
-            <Skeleton className="h-8 w-48"/>
+            <Skeleton className="h-8 w-48" />
           </CardHeader>
           <CardContent>
             <div className="h-[500px] w-full flex items-center justify-center">
@@ -35,34 +64,51 @@ const TransactionPage = () => {
           </CardContent>
         </Card>
       </div>
-    )
+    );
+  }
+
+  if (variant === VARIANTS.IMPORT) {
+    return (
+      <>
+        <ImportCard data={importResults.data} onCancel={onCancelImport} />
+      </>
+    );
   }
 
   return (
     <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
       <Card className="border-none drop-shadow-sm">
         <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
-          <CardTitle className="text-xl line-clamp-1">Transaction History</CardTitle>
-          <Button size="sm" onClick={newTransaction.onOpen}>
-            <Plus className="size-4 mr-2"/>
-            Add New
-          </Button>
+          <CardTitle className="text-xl line-clamp-1">
+            Transaction History
+          </CardTitle>
+          <div className="flex items-center gap-2 flex-col lg:flex-row">
+            <Button
+              size="sm"
+              onClick={newTransaction.onOpen}
+              className="w-full lg:w-auto"
+            >
+              <Plus className="size-4 mr-2" />
+              Add New
+            </Button>
+            <UploadButton onUpload={onUploadCSV} />
+          </div>
         </CardHeader>
         <CardContent>
           <DataTable
-            filterKey="date"
+            filterKey="payee"
             columns={columns}
             data={transactions}
-            onDelete={ (rows)=>{
-                const ids = rows.map((row)=>row.original.id);
-                deleteTransactions.mutate({ids});
+            onDelete={(rows) => {
+              const ids = rows.map((row) => row.original.id);
+              deleteTransactions.mutate({ ids });
             }}
             disabled={isDisabled}
           />
         </CardContent>
       </Card>
     </div>
-  )
-}
+  );
+};
 
 export default TransactionPage;
