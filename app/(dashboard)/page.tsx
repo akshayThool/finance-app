b7918720dcdf16927bce04ae14fbeab45100a127
Akshay <akshay.thool@gmail.com>
2025-03-10 01:16:41 +0530

    Add Data Grid in Dashboard

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,15 +1,9 @@
-"use client";
-import {Button} from "@/components/ui/button";
-import {useNewAccount} from "@/features/accounts/hooks/use-new-account";
+import DataGrid from "@/components/dashboard/data-grid";
 
-export default function Home() {
-  const { onOpen } = useNewAccount();
-
-  return (
-    <div>
-      <Button onClick={onOpen}>
-        Add new Account
-      </Button>
-    </div>
-  );
+export default function DashboardPage() {
+    return (
+        <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+            <DataGrid />
+        </div>
+    );
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,66 +1,100 @@
 import { type ClassValue, clsx } from "clsx";
-import { eachDayOfInterval, isSameDay } from "date-fns";
+import { eachDayOfInterval, format, isSameDay, subDays } from "date-fns";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+    return twMerge(clsx(inputs));
 }
 
 export function convertAmountToMiliunits(amount: number) {
-  return Math.round(amount * 1000);
+    return Math.round(amount * 1000);
 }
 
 export function convertMiliunitsToAmount(miliunits: number) {
-  return miliunits / 1000;
+    return miliunits / 1000;
 }
 
 export function formatCurrency(value: number) {
-  return Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2,
-  }).format(value);
+    return Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+        minimumFractionDigits: 2,
+    }).format(value);
 }
 
 export function calculatePercentageChange(current: number, previous: number) {
-  if (previous === 0) {
-    return previous === current ? 0 : 100;
-  }
+    if (previous === 0) {
+        return previous === current ? 0 : 100;
+    }
 
-  return ((current - previous) / Math.abs(previous)) * 100;
+    return ((current - previous) / Math.abs(previous)) * 100;
 }
 
 export function fillMissingDays(
-  activeDays: {
-    date: Date;
-    income: number;
-    expense: number;
-  }[], 
-  startDate: Date,
-  endDate: Date
+    activeDays: {
+        date: Date;
+        income: number;
+        expense: number;
+    }[],
+    startDate: Date,
+    endDate: Date
 ) {
-  if(activeDays.length === 0){
-    return [];
-  }
-
-  const allDays = eachDayOfInterval({
-    start: startDate,
-    end: endDate
-  });
-
-  const transactionsByDays = allDays.map((day) => {
-    const found = activeDays.find((d) => isSameDay(d.date, day));
-
-    if(found){
-      return found;
-    } else {
-      return {
-        date: day,
-        income: 0,
-        expense: 0
-      }
+    if (activeDays.length === 0) {
+        return [];
     }
-  });
 
-  return transactionsByDays;
+    const allDays = eachDayOfInterval({
+        start: startDate,
+        end: endDate,
+    });
+
+    const transactionsByDays = allDays.map((day) => {
+        const found = activeDays.find((d) => isSameDay(d.date, day));
+
+        if (found) {
+            return found;
+        } else {
+            return {
+                date: day,
+                income: 0,
+                expense: 0,
+            };
+        }
+    });
+
+    return transactionsByDays;
+}
+
+type Period = {
+    from: string | Date | undefined;
+    to: string | Date | undefined;
+};
+
+export function formatDateRange(period?: Period) {
+    const defaultTo = new Date();
+    const defaultFrom = subDays(defaultTo, 30);
+
+    if (!period?.from) {
+        return `${format(defaultFrom, "LLL dd")} - ${format(defaultTo, "LLL dd, y")}`;
+    }
+    if (period.to) {
+        return `${format(period.from, "LLL dd")} - ${format(period.to, "LLL dd, y")}`;
+    }
+
+    return format(period.from, "LLL dd, y");
+}
+
+export function formatPercentage(
+    value: number,
+    options: { addPrefix?: boolean } = { addPrefix: false }
+) {
+    const result = new Intl.NumberFormat("en-US", {
+        style: "percent",
+    }).format(value / 100);
+
+    if (options.addPrefix && value > 0) {
+        return `+${result}`;
+    }
+
+    return result;
 }
