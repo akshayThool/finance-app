7f2a207aecef469aea7081377a2eb21a47c0a98b
Akshay <akshay.thool@gmail.com>
2025-03-09 10:56:32 +0530

    Create Summary Api

--- features/transactions/components/transaction-form.tsx
+++ features/transactions/components/transaction-form.tsx
@@ -1,16 +1,23 @@
-import {z} from "zod";
-import {insertTransactionSchema} from "@/db/schema";
-import {useForm} from "react-hook-form";
-import {zodResolver} from "@hookform/resolvers/zod";
-import {Form, FormControl, FormField, FormItem, FormLabel} from "@/components/ui/form";
-import {Input} from "@/components/ui/input";
-import {Button} from "@/components/ui/button";
-import {Trash} from "lucide-react";
+import { z } from "zod";
+import { insertTransactionSchema } from "@/db/schema";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Trash } from "lucide-react";
 import { Select } from "@/components/form/select";
 import { DatePicker } from "@/components/form/date-picker";
 import { Textarea } from "@/components/ui/textarea";
-import {AmountInput} from "@/components/form/amount-input";
+import { AmountInput } from "@/components/form/amount-input";
 import { convertAmountToMiliunits } from "@/lib/utils";
+import { format, parse } from "date-fns";
 
 const formSchema = z.object({
   date: z.coerce.date(),
@@ -19,7 +26,7 @@ const formSchema = z.object({
   payee: z.string(),
   amount: z.string(),
   notes: z.string().nullable().optional(),
-})
+});
 
 const apiFormSchema = insertTransactionSchema.omit({
   id: true,
@@ -29,16 +36,16 @@ type FormValues = z.input<typeof formSchema>;
 type ApiFormValues = z.input<typeof apiFormSchema>;
 
 type Props = {
-  id?: string,
-  defaultValues?: FormValues,
-  onSubmit: (values: ApiFormValues) => void,
-  onDelete?: () => void,
-  disabled?: boolean,
-  onCreateCategory: (name: string) => void,
-  categoriesOptions: {label: string; value: string}[],
-  onCreateAccount: (name: string) => void,
-  accountsOptions: {label: string; value: string}[]
-}
+  id?: string;
+  defaultValues?: FormValues;
+  onSubmit: (values: ApiFormValues) => void;
+  onDelete?: () => void;
+  disabled?: boolean;
+  onCreateCategory: (name: string) => void;
+  categoriesOptions: { label: string; value: string }[];
+  onCreateAccount: (name: string) => void;
+  accountsOptions: { label: string; value: string }[];
+};
 
 export const TransactionForm = ({
   id,
@@ -49,10 +56,11 @@ export const TransactionForm = ({
   onCreateAccount,
   categoriesOptions,
   onCreateCategory,
-  accountsOptions}: Props) => {
+  accountsOptions,
+}: Props) => {
   const form = useForm<FormValues>({
     resolver: zodResolver(formSchema),
-    defaultValues: defaultValues
+    defaultValues: defaultValues,
   });
 
   const handleSubmit = (values: FormValues) => {
@@ -61,22 +69,24 @@ export const TransactionForm = ({
 
     onSubmit({
       ...values,
-      amount: convertedToMiliunits
+      amount: convertedToMiliunits,
     });
-  }
+  };
 
   const handleDelete = () => {
     onDelete?.();
-  }
+  };
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(handleSubmit)}
-       className="space-y-4 pt-4">
-        <FormField 
+      <form
+        onSubmit={form.handleSubmit(handleSubmit)}
+        className="space-y-4 pt-4"
+      >
+        <FormField
           name="date"
           control={form.control}
-          render={({field}) => (
+          render={({ field }) => (
             <FormItem>
               <FormControl>
                 <DatePicker
@@ -88,16 +98,14 @@ export const TransactionForm = ({
             </FormItem>
           )}
         />
-        <FormField 
+        <FormField
           name="accountId"
           control={form.control}
-          render={({field}) => (
+          render={({ field }) => (
             <FormItem>
-              <FormLabel>
-                Account
-              </FormLabel>
+              <FormLabel>Account</FormLabel>
               <FormControl>
-                <Select 
+                <Select
                   placeholder="Select an Account"
                   options={accountsOptions}
                   onCreate={onCreateAccount}
@@ -109,16 +117,14 @@ export const TransactionForm = ({
             </FormItem>
           )}
         />
-        <FormField 
+        <FormField
           name="categoryId"
           control={form.control}
-          render={({field}) => (
+          render={({ field }) => (
             <FormItem>
-              <FormLabel>
-                Category
-              </FormLabel>
+              <FormLabel>Category</FormLabel>
               <FormControl>
-                <Select 
+                <Select
                   placeholder="Select a Category"
                   options={categoriesOptions}
                   onCreate={onCreateCategory}
@@ -130,56 +136,50 @@ export const TransactionForm = ({
             </FormItem>
           )}
         />
-        <FormField 
+        <FormField
           name="payee"
           control={form.control}
-          render={({field}) => (
+          render={({ field }) => (
             <FormItem>
-              <FormLabel>
-                Payee
-              </FormLabel>
+              <FormLabel>Payee</FormLabel>
               <FormControl>
                 <Input
-                 {...field}
-                 disabled={disabled}
-                 placeholder="Add a Payee"
+                  {...field}
+                  disabled={disabled}
+                  placeholder="Add a Payee"
                 />
               </FormControl>
             </FormItem>
           )}
         />
-        <FormField 
+        <FormField
           name="amount"
           control={form.control}
-          render={({field}) => (
+          render={({ field }) => (
             <FormItem>
-              <FormLabel>
-                Amount
-              </FormLabel>
+              <FormLabel>Amount</FormLabel>
               <FormControl>
                 <AmountInput
-                 {...field}
-                 placeholder="0.00"
-                 disabled={disabled}
+                  {...field}
+                  placeholder="0.00"
+                  disabled={disabled}
                 />
               </FormControl>
             </FormItem>
           )}
         />
-        <FormField 
+        <FormField
           name="notes"
           control={form.control}
-          render={({field}) => (
+          render={({ field }) => (
             <FormItem>
-              <FormLabel>
-                Notes
-              </FormLabel>
+              <FormLabel>Notes</FormLabel>
               <FormControl>
                 <Textarea
-                 {...field}
-                 value={field.value ?? ""}
-                 disabled={disabled}
-                 placeholder="Optional Notes"
+                  {...field}
+                  value={field.value ?? ""}
+                  disabled={disabled}
+                  placeholder="Optional Notes"
                 />
               </FormControl>
             </FormItem>
@@ -188,17 +188,19 @@ export const TransactionForm = ({
         <Button className="w-full" disabled={disabled}>
           {id ? "Save Changes" : "Create a Transaction"}
         </Button>
-        { !!id && <Button
-          type="button"
-          disabled={disabled}
-          onClick={handleDelete}
-          className="w-full"
-          variant="outline"
-        >
-          <Trash className="size-4 mr-2" />
-          Delete Transaction
-        </Button>}
+        {!!id && (
+          <Button
+            type="button"
+            disabled={disabled}
+            onClick={handleDelete}
+            className="w-full"
+            variant="outline"
+          >
+            <Trash className="size-4 mr-2" />
+            Delete Transaction
+          </Button>
+        )}
       </form>
     </Form>
-  )
+  );
 };
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,22 +1,66 @@
-import {type ClassValue, clsx} from "clsx"
-import {twMerge} from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { eachDayOfInterval, isSameDay } from "date-fns";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
-export function convertAmountToMiliunits(amount: number){
+export function convertAmountToMiliunits(amount: number) {
   return Math.round(amount * 1000);
 }
 
-export function convertMiliunitsToAmount(miliunits:number){
+export function convertMiliunitsToAmount(miliunits: number) {
   return miliunits / 1000;
 }
 
-export function formatCurrency(value : number) {
+export function formatCurrency(value: number) {
   return Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
-    minimumFractionDigits: 2
+    minimumFractionDigits: 2,
   }).format(value);
+}
+
+export function calculatePercentageChange(current: number, previous: number) {
+  if (previous === 0) {
+    return previous === current ? 0 : 100;
+  }
+
+  return ((current - previous) / Math.abs(previous)) * 100;
+}
+
+export function fillMissingDays(
+  activeDays: {
+    date: Date;
+    income: number;
+    expense: number;
+  }[], 
+  startDate: Date,
+  endDate: Date
+) {
+  if(activeDays.length === 0){
+    return [];
+  }
+
+  const allDays = eachDayOfInterval({
+    start: startDate,
+    end: endDate
+  });
+
+  const transactionsByDays = allDays.map((day) => {
+    const found = activeDays.find((d) => isSameDay(d.date, day));
+
+    if(found){
+      return found;
+    } else {
+      return {
+        date: day,
+        income: 0,
+        expense: 0
+      }
+    }
+  });
+
+  return transactionsByDays;
 }
